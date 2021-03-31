5e717fa17a2e541f6a547b7dff6f522d4b819b6c
BALAJI SARVEPALLI <sarvepallibalu@gmail.com>
2021-03-31 11:48:08 -0500

    added display results

--- routes/pens.js
+++ routes/pens.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results pens' });
+  res.render('pens', { title: 'Search Results Pens' });
 });
 
 module.exports = router;
